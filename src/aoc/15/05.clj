1c34c363f770e36f22e71757d143293a90d3de79
Florian Kretlow <florian.kretlow@innoq.com>
2025-01-10 17:04:51 +0100

    15/05 part 2

--- src/aoc/15/05.clj
+++ src/aoc/15/05.clj
@@ -1,9 +1,9 @@
-(ns aoc.15.05 
+(ns aoc.15.05
   (:require
    [aoc.lib.io :refer [get-puzzle-input]]
    [clojure.string :as str]))
 
-(defn nice? [s]
+(defn- nice? [s]
   (let [{:keys [vowels has-pair? has-forbidden?]}
         (reduce (fn [{:keys [prev], :as state} c]
                   (if (#{"ab" "cd" "pq" "xy"} (str prev c))
@@ -17,7 +17,33 @@
 
 (defn part-1 [words] (count (filter nice? words)))
 
-(let [words (str/split-lines (get-puzzle-input 15 5))]
-  (println "Part 1:" (time (part-1 words))))
+(defn- identical-pairs-reducer [{:keys [success? pairs prev], :as state} [i c]]
+  (if success?
+    state
+    (let [state' (assoc state :prev c)]
+      (if (= 0 i)
+        state'
+        (let [pair (str prev c), i-pair (dec i)]
+          (if (some #(<= 2 (- i-pair %)) (get pairs pair))
+            {:success? true}
+            (update-in state' [:pairs pair] #(conj % i-pair))))))))
 
-(partition 2 1 "abcd")
+(defn- separated-twins-reducer [{:keys [success? pprev prev], :as state} [_ c]]
+  (cond
+    success? state
+    (= pprev c) {:success? true}
+    :else {:pprev prev, :prev c}))
+
+(defn- nicer? [s]
+  (->> (map vector (range) s)
+       (reduce (fn [[identical-pairs-state separated-twins-state] [i c]]
+                 [(identical-pairs-reducer identical-pairs-state [i c])
+                  (separated-twins-reducer separated-twins-state [i c])])
+               nil)
+       (every? :success?)))
+
+(defn part-2 [words] (count (filter nicer? words)))
+
+(let [words (str/split-lines (get-puzzle-input 15 5))]
+  (println "Part 1:" (time (part-1 words)))
+  (println "Part 2:" (time (part-2 words))))
