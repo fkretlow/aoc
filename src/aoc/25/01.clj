b4de7db39d33ab9a61f048f96bb4f1fcc50bbbf8
Florian Kretlow <florian.kretlow@innoq.com>
2025-12-02 13:31:13 +0100

    25/01.2

--- src/aoc/25/01.clj
+++ src/aoc/25/01.clj
@@ -15,18 +15,54 @@ L99
 R14
 L82"))
 
-(defn- parse [input]
+(defn parse
+  "Given an input string consisting of lines matching the pattern `(L|R)\\d+`,
+  parse it into a seq of numbers, e.g. `L68 -> -68`, `R48 -> 48`."
+  [input]
   (->> (str/split-lines input)
-       (map #(-> [(case (first %) \L - \R +) (parse-long (subs % 1))]))))
+       (map #(* (case (first %) \L -1 \R 1) (parse-long (subs % 1))))))
 
-(defn part-1 [ops]
-  (->> (reductions (fn [x [op y]] (mod (op x y) 100)) 50 ops)
+(defn part-1 [dxs]
+  (->> (reductions (fn [x dx] (mod (+ x dx) 100)) 50 dxs)
        (filter zero?)
        count))
 
-(def ^:private test? true)
+(defn- clicks-at-0
+  "Assuming the dial is pointing at `x` and we rotate it by `dx`, calculate
+  how many times the dial will be pointing at 0 along the way, including at
+  the end of the rotation."
+  [x dx]
+  (cond
+    (zero? dx) 0
+
+    (pos? dx)
+    (loop [c0s 0, x x, dx dx]
+      (let [d0 (- 100 x)]
+        (cond
+          (< dx d0) c0s,
+          (= dx d0) (inc c0s),
+          (> dx d0) (recur (inc c0s) 0 (- dx d0)))))
+
+    (neg? dx)
+    (loop [c0s 0, x (if (zero? x) 100 x), dx dx]
+      (let [d0 (- x)]
+        (cond
+          (> dx d0) c0s,
+          (= dx d0) (inc c0s)
+          (< dx d0) (recur (inc c0s) 100 (- dx d0)))))))
+
+(defn part-2 [dxs]
+  (->> (reduce
+        (fn [[x c0s] dx] [(mod (+ x dx) 100) (+ c0s (clicks-at-0 x dx))])
+        [50 0]
+        dxs)
+       second))
+
+(def ^:private test? false)
 
 (let [input (if test? test-input (get-puzzle-input 25 1))
       ops (parse input)]
-  (part-1 ops))
+  (println "Part 1: " (time (part-1 ops)))
+  (println "Part 2: " (time (part-2 ops))))
+
 
